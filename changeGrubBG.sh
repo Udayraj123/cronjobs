7e7d973bd6898072fbb394330f79163607e4a87a
udayraj123 <udayrajdeshmukh7@gmail.com>
2018-06-15 13:34:47 +0530

    best changeBG sh till now

--- changeGrubBG.sh
+++ changeGrubBG.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#  Running as one time : to set/change grub bg
+if [ $EUID -ne 0 ]; then
+    echo "Please run as root";
+    exit 1;
+fi
+# Running in cron mode
 #  NOTE: Run this cron job as root as it requires sudo privileges. Use the following command-
 #     sudo -u <your-username> crontab -e
 #  And put the following line in it.
@@ -7,29 +12,62 @@
 # ^ For different settings, checkout https://crontab.guru/
 # Also make sure that there are no image files in /boot/grub (else that image will get selected)
 
-#Configure this-
-USER_NAME=udayraj
+# https://unix.stackexchange.com/questions/11470/how-to-get-the-name-of-the-user-that-launched-sudo?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+USER_NAME=$SUDO_USER
 USER_HOME=/home/$USER_NAME
+# TODO- resolve - This line is expected to exist already in /etc/default/grub
 
-# This line is expected to exist already in /etc/default/grub
-# GRUB_BACKGROUND="some/random/path"
-CURR_FILE=$(cat /etc/default/grub | grep BACKGROUND) # Get grub current line
-CURR_FILE=$(cut -d "=" -f 2 <<< "$CURR_FILE")        # File name only
-CURR_FILE=$(echo "$CURR_FILE" | tr -d '"')           # Remove double quotes
 IMG_DIR=$USER_HOME/Pictures/grubs;
-echo "Image directory '$IMG_DIR'";
+# Check directory exists
+if [ ! -d $IMG_DIR ]; then
+    echo "Directory '$IMG_DIR' doesn't exist. Creating now:"
+    mkdir $IMG_DIR;
+    echo "Put your backgrounds for grub there!"
+    exit 1;
+fi
+
+# line is:  GRUB_BACKGROUND="/path/to/your/bg.jpg"
+CURR_FILE=$(cat /etc/default/grub | grep BACKGROUND) # Get grub current line
+# Check BG line exists
+if [[ "$CURR_FILE" == "" ]]; then
+    echo "No previous bg set. Adding a dummy image:";
+    cp ./dummy.jpg $IMG_DIR/dummy.jpg
+    chmod 777 $IMG_DIR/dummy.jpg;
+    echo "GRUB_BACKGROUND=$IMG_DIR/dummy.jpg" | sudo tee -a /etc/default/grub
+fi
+CURR_FILE=$(cat /etc/default/grub | grep BACKGROUND) # Get grub current line
+CURR_FILE=$(cut -d "=" -f 2 <<< "$CURR_FILE")        # File path only
+CURR_FILE=$(echo "$CURR_FILE" | tr -d '"')           # Remove double quotes
+
+echo 
+echo "Searching in Image directory $_green '$IMG_DIR' $_reset";
 ls $IMG_DIR;
-echo $(date +%d/%m' '%T) ": Found current bg as : '$CURR_FILE'";
-for ALL_FILES in $IMG_DIR/*; do # Loop through every file
-    if [[ "$FIRST_FILE" == "" ]]; then
-        FIRST_FILE="$ALL_FILES"
-    elif [[ "$MATCH_FILE" != "" ]]; then
-        NEXT_FILE="$ALL_FILES"
-        break # We've got it!
-    fi
-    if [[ "$CURR_FILE" == "$ALL_FILES" ]]; then
-        MATCH_FILE="$ALL_FILES" # We found our current file entry
-    fi
+echo 
+echo $(date +%d/%m' '%T) ": Found current bg as : $_green '$CURR_FILE' $_reset";
+
+shopt -s nullglob # set nullglob; If no files exist, make the string null!
+JPG_IMAGES="$IMG_DIR/*.jpg"
+PNG_IMAGES="$IMG_DIR/*.png"
+ALL_IMAGES="$JPG_IMAGES $PNG_IMAGES";
+# TODO - Remove this dependency-
+shopt -u nullglob # make it back to unset!
+
+if [[ "$ALL_IMAGES" == " " ]]; then
+    echo "No images found in $IMG_DIR!";
+    exit 1;
+fi
+for LOOP_FILE in $ALL_IMAGES; do # Loop through every file
+if [[ "$FIRST_FILE" == "" ]]; then
+    FIRST_FILE="$LOOP_FILE"
+elif [[ "$MATCH_FILE" != "" ]]; then
+    NEXT_FILE="$LOOP_FILE"
+     # We've got it!
+     break
+ fi
+ if [[ "$CURR_FILE" == "$LOOP_FILE" ]]; then
+    # We found our current bg 
+    MATCH_FILE="$LOOP_FILE" 
+fi
 done
 
 # If $NEXT_FILE empty we hit end of list so use First file name
@@ -41,14 +79,14 @@ NEXT_FILE_NAME=$(basename "$NEXT_FILE");
 msg="$(date +%d/%m' '%T) : Changed bg image to '$NEXT_FILE_NAME'";
 echo $msg;
 
-# replace background file name in grub source file
+# replace background file path in grub source file
 sed -i "s|$CURR_FILE|$NEXT_FILE|g" /etc/default/grub
 
 # Send a notification
 DISPLAY=:0.0 #needed when inside cron 
 su $USER_NAME -c "notify-send \"Grub BG Changer\" \"Changed bg image to '$NEXT_FILE_NAME'\""
 # ^Single quotes won't decode the variables beforehand
-echo $msg >> $USER_HOME/cronJobs/cronLog
+echo $msg >> ./cronLog
 
 # replace background file name in grub configuration file
 # ^Short cut so we don't have to run `sudo update-grub`
@@ -56,4 +94,4 @@ echo $msg >> $USER_HOME/cronJobs/cronLog
 # cp /boot/grub/grub.cfg /boot/grub/grub.cfg~
 # sed -i "s|$CURR_FILE|$NEXT_FILE|g" /boot/grub/grub.cfg
 
-update-grub >> $USER_HOME/cronJobs/cronLog
+update-grub >> ./cronLog
